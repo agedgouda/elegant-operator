d94ad18afde826353953fb70d0dd3ea9f4b461a1
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2022-06-27 19:40:52 -0700

    clean up some injection nonsense

--- src/mail-handler/mail-handler.module.ts
+++ src/mail-handler/mail-handler.module.ts
@@ -3,10 +3,9 @@ import { MailHandlerService } from './mail-handler.service';
 import { MailHandlerController } from './mail-handler.controller';
 import { MailModule } from 'src/mail/mail.module';
 import { HttpModule } from '@nestjs/axios';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MailModule,HttpModule,ConfigModule],
+  imports: [MailModule,HttpModule],
   providers: [MailHandlerService],
   controllers: [MailHandlerController]
 })
