a3e6e1ea775c48097cb4fe1da87c6429e85e73d4
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2022-07-20 10:58:44 -0700

    abad url somewheres

--- src/hostfully/hostfully.service.ts
+++ src/hostfully/hostfully.service.ts
@@ -16,9 +16,9 @@ export class HostfullyService {
     async hostfullyPropertyUpdate(hostfullyID) {
         
         const hostfullyData = await this.getHostfullyProperty(hostfullyID);
-        const index = await this.algoliaService.initIndex(this.configService.get<string>('ALGOLIA_INDEX'));
-        const oldRecord = await index.getObject(hostfullyID);
-        const algoliaUpdate = await index.partialUpdateObject(hostfullyData, {createIfNotExists: true});
+        //const index = await this.algoliaService.initIndex(this.configService.get<string>('ALGOLIA_INDEX'));
+        //const oldRecord = await index.getObject(hostfullyID);
+        /*const algoliaUpdate = await index.partialUpdateObject(hostfullyData, {createIfNotExists: true});
         
         const updateLog = {
             "hostfullyData":hostfullyData,
@@ -29,6 +29,9 @@ export class HostfullyService {
         console.log(updateLog);
         
         return updateLog;
+        */
+       console.log(hostfullyData);
+       return "testing";
     }
 
     async algoliaTest() {
@@ -40,7 +43,7 @@ export class HostfullyService {
 
 
     private async getHostfullyProperty(hostfullyID) {
-        
+        console.log(this.configService.get<string>('HOSTFULL_API_URL')+"properties/"+hostfullyID);
         const hostfullyProperty = await this.getHostfullyData(this.configService.get<string>('HOSTFULL_API_URL')+"properties/"+hostfullyID) ;
         const hostfullyPropertyOwnership = await this.getHostfullyData(this.configService.get<string>('HOSTFULL_API_URL')+"propertyownership/"+hostfullyID) ;
         const hostfullyPropertyOwner = await this.getHostfullyData(this.configService.get<string>('HOSTFULL_API_URL')+"owners/"+hostfullyPropertyOwnership.ownerUid) ;
